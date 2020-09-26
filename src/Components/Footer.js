89ab30b2fd1d8dc4e9e5310c10a95fe855be79ba
Aravind Kumar <aravindhalahalli.81@gmail.com>
2020-09-26 21:31:33 +0530

    make it better with host

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -17,9 +17,10 @@ class Footer extends Component {
            <ul className="social-links">
               {networks}
            </ul>
+
            <ul className="copyright">
-              <li>&copy;Copyright 2020</li>
-              <li><a title="Styleshout" href="http://www.styleshout.com/">Aravind Kumar</a></li>
+              <li>&copy;Copyright 2020 Aravind Kumar</li>
+              {/* <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li> */}
            </ul>
 
         </div>
